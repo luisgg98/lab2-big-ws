0aca469c710d294229884a90828d53704d6ae42f
Luis Garcia <luisgarcia98@hotmail.es>
2020-10-25 17:05:09 +0100

    description is singular

--- src/main/java/translator/WebServiceConfig.java
+++ src/main/java/translator/WebServiceConfig.java
@@ -9,6 +9,7 @@ import org.springframework.web.servlet.HttpServletBean;
 import org.springframework.ws.config.annotation.EnableWs;
 import org.springframework.ws.config.annotation.WsConfigurerAdapter;
 import org.springframework.ws.transport.http.MessageDispatcherServlet;
+/*que hago aqui con wsdl20?*/
 import org.springframework.ws.wsdl.wsdl11.DefaultWsdl11Definition;
 import org.springframework.ws.wsdl.wsdl11.SimpleWsdl11Definition;
 import org.springframework.xml.xsd.SimpleXsdSchema;
@@ -32,3 +33,13 @@ public class WebServiceConfig extends WsConfigurerAdapter {
     return s;
   }
 }
+
+/*Enlaces de interés**/
+//  https://glenmazza.net/blog/entry/switch-soap11-to-soap12
+// https://docs.spring.io/spring-ws/docs/current/reference/
+// https://www.w3.org/TR/soap12/
+// https://moodle.unizar.es/add/pluginfile.php/2739988/mod_resource/content/6/IW_T4.1_Big_Web_Services.pdf
+// https://stackoverflow.com/questions/6467234/how-to-convert-a-wsdl-file-made-for-soap-1-1-to-support-soap-1-2
+
+
+
